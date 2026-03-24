5eb5f8c2ccaae7473bc2ef05f5ecaf9b3c82a6e2
userhelpguide <babajimaharaji@outlook.com>
2026-03-24 12:57:11 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,3 +18,14 @@ html_static_path = ['_static']
 # Custom JS & Favicon
 html_js_files = ['chatbot.js']  # chatbot widget
 html_favicon = '_static/favicon.png'
+
+# Google & Bing Verification Meta Tags
+html_context = {
+    "meta_tags": """
+    <meta name="google-site-verification" content="vWuKdfn7mBaw-KlLRh8j_GtlIs4FQ-h0VcG-WO8D8ns" />
+  <meta name="msvalidate.01" content="649552A7F8FC8C674CD07172F20F2D02" />
+    """
+}
+
+# Base URL for sitemap
+html_baseurl = 'https://matichub-pc.readthedocs.io/en/latest'
